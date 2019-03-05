76a3f3db60d6532473f1e27ea1d2e85c16cd4b0f
Kuttiya <Siwadol.Kuttiya@thomsonreuters.com>
2019-03-05 18:10:31 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const { app,BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+}
+app.on('ready', createWindow);
